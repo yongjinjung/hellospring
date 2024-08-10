1f8bf83f09f21ede5be026af29439a134bd6affd
yongjinjung <jyj1641@gmail.com>
2024-08-10 22:32:09 +0900

    클라이언트 주석처리

--- src/main/java/tobyspring/hellospring/payment/Client.java
+++ src/main/java/tobyspring/hellospring/payment/Client.java
@@ -7,6 +7,11 @@ import tobyspring.hellospring.SimpleExRateProvider;
 import java.math.BigDecimal;
 
 public class Client {
+    /**
+     * 클라이언트 
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
         PaymentService paymentService = new PaymentService(new SimpleExRateProvider());
         Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
