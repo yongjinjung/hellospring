b20907b26f5e21212d0a8380c98936759ddc332b
youngjinjung <jyj1641@gmail.com>
2024-08-19 20:14:41 +0900

    로또 생성기 개발 완료

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -6,20 +6,29 @@ import java.util.Random;
 public class LottoGenerator {
     private final Random random = new Random();
     private int[] lottoNumbers;
-    private int count;
 
     public int[] gennerate(){
-        
         lottoNumbers = new int[6];
-        count = 0;
-        
+        int count = 0;
+
         while(count < 6){
             //1부터 45 사이의 숫자 생성
             int number = random.nextInt(45) + 1;
-            //
+            //중복되지 않은 경우에만 배열에 추가
+            if(isUnique(number)){
+                lottoNumbers[count] = number;
+                count++;
+            }
         }
-
         return Arrays.stream(lottoNumbers).sorted().toArray();
+    }
 
+    private boolean isUnique(int number) {
+        for(int i = 0 ; i < 6 ; i++){
+            if(number == lottoNumbers[i]){
+                return false;
+            }
+        }
+        return true;
     }
 }
