b44377307585069787340bf9d01dc153b1f336b4
youngjinjung <jyj1641@gmail.com>
2024-10-09 19:28:05 +0900

    시간정보에 대한 테스트
    LocalDateTime.now() 를 이용한 현재 시간을 테스트
    Clock.fixed() 를 이용한 고정된 시간을 테스트

---
+++ src/test/java/tobyspring/learningtest/ClockTest.java
@@ -0,0 +1,49 @@
+package tobyspring.learningtest;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+public class ClockTest {
+
+    //Clock을 이용해서 LocalDateTime.now() ?
+    @Test
+    void clock(){
+        Clock clock = Clock.systemDefaultZone();
+
+        LocalDateTime now = LocalDateTime.now(clock);
+        System.out.println("now = " + now);
+
+        System.out.println("LocalDateTime = " + LocalDateTime.now());
+
+
+    }
+
+    @Test
+    void clock2(){
+        Clock clock = Clock.systemDefaultZone();
+        LocalDateTime dt1 = LocalDateTime.now(clock);
+        LocalDateTime dt2 = LocalDateTime.now(clock);
+
+        Assertions.assertThat(dt2).isAfter(dt1);
+    }
+
+    //Clock을 Test에서 사용할 때 내가 원하는 시간을 지정해서 현재 시간을 가져올게 할 수 있는가?
+    @Test
+    void fixedClock(){
+        Clock clock = Clock.fixed(Instant.now(), ZoneId.systemDefault());
+
+        LocalDateTime dt1 = LocalDateTime.now(clock);
+        LocalDateTime dt2 = LocalDateTime.now(clock);
+        LocalDateTime dt3 = LocalDateTime.now(clock).plusHours(1);
+
+        Assertions.assertThat(dt3).isEqualTo(dt1.plusHours(1));
+        Assertions.assertThat(dt3).isEqualTo(dt1.plusHours(1));
+
+
+    }
+}
