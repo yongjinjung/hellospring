22a06cb99a34bed5757908c034c942c630d1e185
youngjinjung <jyj1641@gmail.com>
2024-10-09 19:26:36 +0900

    스프링 DI를 이용하는 테스트 테스트용 협력자(Collaborator)/의존 오브젝트를 스프링의 구성 정보를 이용해서 지정하고 컨테이너로 부터 테스트 대상을 가져와서 테스트
    @ContextConfiguration(test 컨텍스트에서 사용하기 위한정보)
    @Autowired

---
+++ src/test/java/tobyspring/hellospring/TestObjectFactory.java
@@ -0,0 +1,24 @@
+package tobyspring.hellospring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import tobyspring.hellospring.payment.ExRateProvider;
+import tobyspring.hellospring.payment.ExRateProviderStub;
+import tobyspring.hellospring.payment.PaymentService;
+
+import java.math.BigDecimal;
+
+@Configuration
+public class TestObjectFactory {
+
+    @Bean
+    public PaymentService paymentService(){
+        return new PaymentService(exRateProvider());
+    }
+
+    @Bean
+    public ExRateProvider exRateProvider(){
+        return new ExRateProviderStub(BigDecimal.valueOf(1_000));
+    }
+}
